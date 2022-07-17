b42a0b75cf21c51f05a21b55163595c3f421884b
yaseerahmed <kitsyaseer@gmail.com>
2022-07-17 22:55:51 +0530

    added housing packge and setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from gettext import install
+from setuptools import setup
+from typing import List
+
+NAME = "ML_Project"
+VERSION = "0.0.1"
+AUTHOR = "Yaseer"
+DESCRIPTION = "This is a self made project"
+PACKAGES = ["housing"]
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+
+def get_requirements_list():
+    '''
+    This function will read the contents of requirements.txt file
+    i.e., all packages and returns it as a strig List
+    '''
+    with open(REQUIREMENTS_FILE_NAME) as rf:
+        return rf.readlines()
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires = get_requirements_list()
+)
