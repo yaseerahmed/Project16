e4512cb49ea48d3c65619035aa2332d222af2e84
yaseerahmed <kitsyaseer@gmail.com>
2022-07-24 15:49:53 +0530

    added configuration file -> housig/config

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,26 +1,27 @@
-
+#import imp
+from housing.entity.config_entity import DataIngestionConfig,DataTransformationConfig,DataValidationConfig,ModelEvaluationConfig,ModelPusherConfig,ModelTrainerConfig,TrainingPipelineConfig
 
 class Configuration:
 
     def __init__(self)->None:
         pass
 
-    def get_data_ingestion_config(self):
+    def get_data_ingestion_config(self)->DataIngestionConfig:
         pass
 
-    def get_data_validation_config(self):
+    def get_data_validation_config(self)->DataValidationConfig:
         pass
 
-    def get_data_transformation_config(self):
+    def get_data_transformation_config(self)->DataTransformationConfig:
         pass
 
-    def get_model_training_config(self):
+    def get_model_training_config(self)->ModelTrainerConfig:
         pass
 
-    def get_model_evaluation_config(self):
+    def get_model_evaluation_config(self)->ModelEvaluationConfig:
         pass
 
-    def get_model_pusher_config(self):
+    def get_model_pusher_config(self)->ModelPusherConfig:
         pass
 
     def get_training_pipeline_config(self):
