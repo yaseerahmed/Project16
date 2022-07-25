158f40827bea857fefd8e001623b6c74be56a6d3
yaseerahmed <kitsyaseer@gmail.com>
2022-07-25 18:42:36 +0530

    added constant folder and completed configuration

---
+++ housing/util/util.py
@@ -0,0 +1,13 @@
+import yaml
+from exception import HousingException
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
